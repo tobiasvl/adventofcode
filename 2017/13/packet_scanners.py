6f8fca0e49eb1919dfaf31e45b474a81b5cc7092
Tobias V. Langhoff <tobias@langhoff.no>
2017-12-13 14:30:53 +0100

    AoC 2017, day 13, part 2
    With refactor of part one

--- 2017/13/packet_scanners.py
+++ 2017/13/packet_scanners.py
@@ -5,35 +5,39 @@ with open('input.txt') as f:
     layers = re.search('(\d*): (\d*)', lines[-1].strip())
     firewall = []
     for i in range(int(layers.group(1)) + 1):
-        firewall.append([])
+        firewall.append(None)
     for line in lines:
         layer = re.search('(\d*): (\d*)', line.strip())
-        firewall[int(layer.group(1))] = [0, int(layer.group(2)), False]
+        firewall[int(layer.group(1))] = int(layer.group(2))
 
 
-def advance(layer):
+def run(delay=0):
+    # If the run is not delayed, return the severity of the run.
+    # If the run is delayed, don't return the actual severity (for
+    # optimizational reasons), but return False if the run is
+    # successful (ie. has no severity) and True if it is unsuccessful
+    # (ie. it has a severity).
     severity = 0
-    try:
-        if firewall[layer][0] == 0:
-            severity += firewall[layer][1] * layer
-    except IndexError:
-        pass
-    for layer in firewall:
-        try:
-            if layer[0] == layer[1] - 1 and layer[2] == False:
-                layer[2] = True
-            elif layer[0] == 0 and layer[2] == True:
-                layer[2] = False
-            if layer[2] == True:
-                layer[0] -= 1
-            else:
-                layer[0] += 1
-        except IndexError:
-            pass
+    for layer in range(len(firewall)):
+        if firewall[layer] is not None:
+            # Instead of the scanner bouncing up and down a layer of
+            # length firewall[layer], we imagine it as a loop of length
+            # 2 * firewall[layer] - 1. That way we can use modulo to
+            # see when it's in position 0.
+            if (delay + layer) % (2 * (firewall[layer] - 1)) == 0:
+                # This is ugly but efficient
+                if delay > 0:
+                    return True
+                severity += firewall[layer] * layer
     return severity
 
 
-severity = 0
-for picosecond in range(len(firewall)):
-    severity += advance(picosecond)
-print "The severity of the trip without delay is %d" % severity
+print "The severity of the trip without delay is %d" % run()
+
+delay = 0
+while True:
+    if not run(delay):
+        break
+    delay += 1
+
+print "The delay needed for a successful run is %d" % delay
