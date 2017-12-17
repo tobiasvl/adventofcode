ba54fa493f8d67615a127a42af85236a2963655e
Tobias V. Langhoff <tobias@langhoff.no>
2017-12-17 20:05:17 +0100

    AoC 2017, day 17, part 2

--- 2017/17/spinlock.py
+++ 2017/17/spinlock.py
@@ -1,11 +1,29 @@
+from collections import deque
+
 step = 349
 
 buffer = [0]
 position = 0
 
-for i in range(1, 2018):
+for i in xrange(1, 2018):
     position = (position + step) % len(buffer)
     buffer.insert(position+1, i)
     position += 1
 
 print "The value following 2017 after 2017 insertions is %d" % buffer[buffer.index(2017) + 1]
+
+# Well, that went well for part 1, but too slow for
+# part 2. There are probably ways to figure this out
+# without brute force, but brute force is surprisingly
+# "fast" with a deque.
+
+buffer = deque([0])
+
+for i in xrange(1, 50000001):
+    buffer.rotate(-step)
+    buffer.append(i)
+
+# Python 2's deque actually doesn't have index().
+# Biggest reason to upgrade yet.
+buffer = list(buffer)
+print "The value following 0 after 50 000 000 insertions is %d" % buffer[buffer.index(0) + 1]
