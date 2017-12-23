aa146aaf7667fa871c054bca72211060df117a6c
Tobias V. Langhoff <tobias@langhoff.no>
2017-12-23 12:37:16 +0100

    AoC 2017, day 23, part 2
    Hand-disassembled version of the patched assembler

--- 2017/23/coprocessor.py
+++ 2017/23/coprocessor.py
@@ -19,14 +19,12 @@ def get_value(x):
         return regs[x]
 
 
-def run(debug=False):
+def run_debug():
     i = 0
     muls = 0
 
     for reg in regs:
         regs[reg] = 0
-    if debug:
-        regs['a'] = 1
 
     while i >= 0 and i < len(instructions):
         instruction = instructions[i]['operation']
@@ -45,6 +43,33 @@ def run(debug=False):
         i += 1
     return muls
 
-print "The mul instruction is invoked %d times" % run()
-#run(True)
-#print regs['h']
+print "The mul instruction is invoked %d times" % run_debug()
+
+# I should probably patch the actual assembler instead
+def run_patched():
+    regs['a'] = 1 # as of now the patch doesn't solve part 1
+    regs['h'] = 0 # return
+    regs['b'] = 81
+    regs['c'] = regs['b']
+    regs['b'] *= 100
+    regs['b'] -= -100000
+    regs['c'] = regs['b'] - -17000
+
+    while True:
+        regs['f'] = 1
+        regs['d'] = 2
+        regs['e'] = 2
+        while True:
+            if regs['b'] % regs['d'] == 0:
+                regs['f'] = 0
+            regs['d'] -= -1
+            if regs['d'] != regs['b']:
+                continue
+            if regs['f'] == 0:
+                regs['h'] -= -1
+            if regs['b'] == regs['c']:
+                return regs['h']
+            regs['b'] -= -17
+            break
+
+print "Register h holds the value %d" % run_patched()
