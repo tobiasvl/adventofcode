0936c2843edffc1950f781c7a4df4161102bc939
Tobias V. Langhoff <tobias@langhoff.no>
2018-11-28 16:09:14 +0100

    AoC 2016 day 14, part 1

---
+++ 2016/14/onetimepad.py
@@ -0,0 +1,24 @@
+import hashlib
+import re
+
+def find(input):
+    index = 0
+    three = re.compile(r'(\w)\1{2}')
+    five = re.compile(r'(\w)\1{4}')
+    potential_keys = dict()
+    keys = set()
+    while True:
+        md5 = hashlib.md5('%s%d' % (input, index)).hexdigest()
+        result = re.search(five, md5)
+        if result:
+            for k, v in potential_keys.iteritems():
+                if index - k <= 1000 and v == result.group(1):
+                    keys.add(k)
+                    if len(keys) == 100:
+                        return sorted(list(keys))[63]
+        result = re.search(three, md5)
+        if result:
+            potential_keys[index] = result.group(1)
+        index += 1
+
+print "This index produces the 64th key: %s" % find('ngcjuoqr')
