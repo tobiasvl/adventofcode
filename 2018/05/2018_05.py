fc8e94b7932d833cf5ef5d1845226a18e3e12133
Tobias V. Langhoff <tobias@langhoff.no>
2018-12-05 15:03:13 +0100

    AoC 2018, day 5 improvement
    Use string as stack instead of deque (which could have been just a list anyway)

--- 2018/05/2018_05.py
+++ 2018/05/2018_05.py
@@ -1,20 +1,18 @@
-import collections
-import string
+from string import ascii_lowercase as lower, ascii_uppercase as upper
 
 with open("input.txt") as f:
     polymer = f.read().strip('\n')
 
 def react(polymer):
-    result = collections.deque()
+    result = ''
     for c in polymer:
-        if len(result) > 0:
-            c2 = result[-1]
-            if c.lower() == c2.lower() and ((c.islower() and c2.isupper()) or (c2.islower() and c.isupper())):
-                result.pop()
-                continue
-        result.append(c)
+        c2 = result[-1:]
+        if c == c2.swapcase():
+            result = result[:-1]
+        else:
+            result += c
     return result
 
 print len(react(polymer))
 
-print min(map(len, [react(polymer.replace(l[0], '').replace(l[1], '')) for l in zip(string.lowercase, string.uppercase)]))
+print min(map(len, [react(polymer.replace(l[0], '').replace(l[1], '')) for l in zip(lower, upper)]))
