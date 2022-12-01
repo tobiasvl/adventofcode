773f5d925d1e4535f315688bfb20e48bcf43a4d8
Tobias V. Langhoff <tobias@langhoff.no>
2022-12-01 22:33:46 +0100

    AoC, day 1: Refactor, more idiomatic

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,43 +1,28 @@
 #![allow(clippy::unwrap_used)]
 
-fn collect_elves(lines: &Vec<String>) -> Vec<Vec<usize>> {
-    let mut elves = Vec::<Vec<usize>>::new();
-    let mut elf = Vec::<usize>::new();
-
-    for line in lines {
-        if line.is_empty() {
-            elves.push(elf);
-            elf = Vec::<usize>::new();
-        } else {
-            elf.push(line.parse().unwrap());
-        }
-    }
-    elves.push(elf);
-
-    elves
+fn collect_elves(lines: &str) -> Vec<Vec<u32>> {
+    lines
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|x| x.parse().unwrap()).collect())
+        .collect()
 }
 
-fn part1(lines: &Vec<String>) -> usize {
-    collect_elves(lines)
-        .iter()
-        .map(|elf| elf.iter().sum::<usize>())
-        .max()
-        .unwrap()
+fn sum_elves(elves: &[Vec<u32>]) -> Vec<u32> {
+    elves.iter().map(|elf| elf.iter().sum()).collect()
 }
 
-fn part2(lines: &Vec<String>) -> usize {
-    let mut elves = collect_elves(lines)
-        .iter()
-        .map(|elf| elf.iter().sum::<usize>())
-        .collect::<Vec<usize>>();
+fn part1(lines: &str) -> u32 {
+    *sum_elves(&collect_elves(lines)).iter().max().unwrap()
+}
 
+fn part2(lines: &str) -> u32 {
+    let mut elves = sum_elves(&collect_elves(lines));
     elves.sort_unstable();
-
     elves.iter().rev().take(3).sum()
 }
 
 fn main() {
-    let lines = util::collect_lines(format!("{}/input", env!("CARGO_PKG_NAME"))).unwrap();
+    let lines = std::fs::read_to_string(format!("{}/input", env!("CARGO_PKG_NAME"))).unwrap();
     println!("{}", part1(&lines));
     println!("{}", part2(&lines));
 }
@@ -45,7 +30,7 @@ fn main() {
 #[test]
 #[allow(clippy::unwrap_used)]
 fn day01() {
-    let lines = util::collect_lines("sample").unwrap();
+    let lines = std::fs::read_to_string("sample").unwrap();
     assert_eq!(part1(&lines), 24000);
     assert_eq!(part2(&lines), 45000);
 }
